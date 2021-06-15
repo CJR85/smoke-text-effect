2c21c345e4c95c1fe6c4aa9d56039010af73e104
Calvin Roberts <roberts.calvin85@gmail.com>
2021-06-15 20:57:49 +0100

    Add active class on hovered span tag.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 // Split text into letters
 const text = document.querySelector('.text');
 text.innerHTML = text.textContent.replace(/\$/g, '<span>$&</span>');
+
+// Add active class on hovered <span> tag
+const letters = document.querySelectorAll('span');
+for (let i = 0; i < letters.length; i++) {
+  letters[i].addEventListener('mouseover', function () {
+    letters[i].classList.add('active');
+  });
+}
